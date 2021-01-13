5bc537cf04e8a142d57975c2903df1bd5e143f98
mary-jalal12 <77390362+mary-jalal12@users.noreply.github.com>
2021-01-13 10:58:57 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime bday = Convert.ToDateTime("1/2/2001");
+            DateTime today = DateTime.Today;
+            int age = today.Year - bday.Year;
+            if (bday > today.AddYears(-age))
+                age--;
+            Console.WriteLine(age);
+            Console.ReadLine();
+ 
+
+
+
+
+        }
+    }
+}
